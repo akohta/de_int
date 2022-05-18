9ade7756666ca7c0214e220a0e1bc8ba79c21a09
akohta <gml.yj21364@gmail.com>
2022-05-18 18:04:12 +0900

    add comments

--- de_int.h
+++ de_int.h
@@ -10,22 +10,22 @@
 
 double complex deintz(double complex (*f)(double,void *),double a,double b,void *pa, double eps,double *err); 
 double         deintd(double         (*f)(double,void *),double a,double b,void *pa, double eps,double *err); 
-/* integral of f(x) over (a,b) 
+/* integral of f(x) over (a,b). f(x) needs to be analytic over (a,b).
+ -- arguments --
  f         : function pointer of integrand 
  a         : lower limit of integration
  b         : upper limit of integration
  pa        : pointer of parameter list 
  eps       : relative error requested 
- *err      : estimation of the absolute error 
- 
- f(x) needs to be analytic over (a,b).
- error message
-   err >= 0 : normal termination. returned value is estimated abosolute error. 
-   err < 0  : abnormal termination (exceed mmax). 
+ err       : error message
+             err >= 0 : normal termination. returned value is an estimate of the abosolute error.
+             err <  0 : abnormal termination. integrated value did not converge within the limit mmax.
 */
 
 double complex deintiz(double complex (*f)(double,void *),double a,void *pa,double eps,double *err);
 double         deintid(double         (*f)(double,void *),double a,void *pa,double eps,double *err);
-/* integral of f(x) over (a,infinity) */
+/* integral of f(x) over (a,infinity). f(x) needs to be analytic over (a,infinity). 
+ arguments are the same as the above.
+*/
 
 #endif
